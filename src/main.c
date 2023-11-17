e8ec301f94147d95b5a8d4ca5ee61c49ffde1bd5
kareem <kareem_rajeh@student.uml.edu>
2023-11-16 19:42:29 -0500

    implemented get_random, determine_winner, and made a enumerated type for result to increase readability.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
-#include <stdbool.h>
+#include <stdio.h>      // for printf and scanf
+#include <stdbool.h>    // for bool
+#include <stdlib.h>     // includes rand
+#include <time.h>       // includes current time
 
 /*
  1. Get users input
@@ -8,49 +10,86 @@
  4. Compare computer with user
     - rock beats scissor
     - paper beats rock
-    - sissor beats paper
+    - scissor beats paper
  */
 
-enum moves {rock=1, paper=2, scissor=3};
+typedef enum {lost=0, won=1} result;
+typedef enum {rock=1, paper=2, scissor=3} move;
 
 bool valid_input(int move);
-int get_input(void);
+move get_input(void);
+move get_random(void);
+result determine_winner(move user, move computer);
 
-int get_input(void)
+// returning won if user won else lost
+result determine_winner(move user, move computer) {
+    if (user == rock && computer == scissor)
+    {
+        return won;
+    }
+    else if (user == paper && computer == rock)
+    {
+        return won;
+    }
+    else if (user == scissor && computer == paper)
+    {
+        return won;
+    }
+  
+    return lost;
+}
+
+move get_random(void)
 {
-    int move = 0;
+    // reset rand to the current time
+    srand(time(0));
+    return (rand() % 3) + 1;
+}
+
+
+move get_input(void)
+{
+    int _move = 0;
     char input;
     do {
         printf("Enter a move (r/p/s): ");
         scanf("%c", &input);
         
         if (input == 'r') {
-            move = rock;
+            _move = rock;
         }
         else if (input == 'p') {
-            move = paper;
+            _move = paper;
         }
         else if (input == 's') {
-            move = scissor;
+            _move = scissor;
         }
-        else move = 0;
-    } while (!valid_input(move));
+        else _move = 0;
+    } while (!valid_input(_move));
     
     
-    return move;
+    return _move;
 }
 
-bool valid_input(int move) {
-    if (move == 0) {
+
+bool valid_input(int _move) {
+    if (_move == 0) {
         return false;
     }
     return true;
 }
 
 
-
 int main (int argc, char* argv[]) {
-    printf("%s\n", argv[1]);
+    printf("Hello Player: %s\n", argv[1]);
+    move player_move = get_input();
+    move computer_move = get_random();
+    result winner = determine_winner(player_move, computer_move);
+    
+    if (winner) {
+        printf("%s won!\n", argv[1]);
+    }
+    else printf("You lost :(\n");
     
     return 0;
 }
